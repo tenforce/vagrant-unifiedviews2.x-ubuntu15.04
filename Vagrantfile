887f6f5fa7bb4cc31b57147d1b845fd02d371a0c
Paul Massey <paul.massey@tenforce.com>
2015-10-23 14:17:02 +0200

    Sesame and Documentation updates.

--- Vagrantfile
+++ Vagrantfile
@@ -3,11 +3,18 @@
 
 Vagrant.configure(2) do |config|
   config.vm.box = "boxcutter/ubuntu1504-desktop"
-  config.vm.provision :shell, path: "bootstrap.sh", binary: false
   config.vm.synced_folder ".", "/vagrant"
-  # config.vm.network "public_network"
 
-  # Default setup
+  # This will attempt to cache downloaded files (only if plugin is
+  # present).
+  
+  if Vagrant.has_plugin?("vagrant-cachier")
+     config.cache.scope = :box
+  end
+
+  config.vm.provision :shell, path: "bootstrap.sh", binary: false
+
+  # Default provider setup
   config.vm.provider "virtualbox" do |vb|
       vb.name = "VagrantUnifiedView2.x-Ubuntu15.04"
       vb.gui = true
@@ -22,8 +29,10 @@ Vagrant.configure(2) do |config|
   end
 
   # Only on linux boxes with libvirt installed
-  #    vagrant up --provider libvirt 
+  #    vagrant up --provider=libvirt
   config.vm.provider :libvirt do |libvirt|
       libvirt.host = "VagrantUnifiedView2.x-Ubuntu15.04"
   end
+
+  
 end
--- bootstrap-sesame.sh
+++ bootstrap-sesame.sh
@@ -2,33 +2,44 @@
 #######################################################################
 # Install the sesame 4.0.0 store.
 
-# SESAME_DOWNLOAD="http://downloads.sourceforge.net/project/sesame/Sesame%204/4.0.0/openrdf-sesame-4.0.0-onejar.jar?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsesame%2F&ts=1445437140&use_mirror=netcologne"
-# mkdir -p /vagrant/downloads
-# pushd /vagrant/downloads
-#   wget -N SESAME_DOWNLOAD
-# popd
-
 if [ -f "/vagrant/downloads/openrdf-sesame-2.8.6-sdk.zip" ]; then
-    service tomcat7 stop
-    service unifiedviews-backend stop
-    service unifiedviews-frontend stop
+    pushd /vagrant/downloads
+     service tomcat7 stop
+     service unifiedviews-backend stop
+     service unifiedviews-frontend stop
     
-    mkdir -p /vagrant/database/unifiedviews
-    unzip /vagrant/downloads/openrdf-sesame-2.8.6-sdk.zip
-    cp /vagrant/downloads/openrdf-sesame-2.8.6/war/*.war /var/lib/tomcat7/webapps/
-    cp /vagrant/config-files/tomcat7-default.sesame /etc/default/tomcat7
-    
-    # Now block updating of tomcat (since config files have been updated)
-    apt-mark hold tomcat7
-    
-    # Replace the default in-memory configuration files
-    cp /vagrant/config-files/frontend-config.properties /etc/unifiedviews/
-    cp /vagrant/config-files/backend-config.properties /etc/unifiedviews/
+     mkdir -p /vagrant/sesame-databases
+     unzip /vagrant/downloads/openrdf-sesame-2.8.6-sdk.zip
+     cp /vagrant/downloads/openrdf-sesame-2.8.6/war/*.war /var/lib/tomcat7/webapps/
+     cp /vagrant/config-files/tomcat7-default.sesame /etc/default/tomcat7
+     cp /vagrant/config-files/proxy.properties.default /vagrant/sesame-databases/openrdf-sesame/conf
 
-    # Restart all necessary services
-    service tomcat7 start
-    service unifiedviews-backend start
-    service unifiedviews-frontend start
+     # Now block updating of tomcat (since config files have been updated)
+     apt-mark hold tomcat7
+    
+     # Replace the default in-memory configuration files
+     cp /vagrant/config-files/frontend-config.properties /etc/unifiedviews/
+     cp /vagrant/config-files/backend-config.properties /etc/unifiedviews/
+
+     # Restart all necessary services
+     service tomcat7 start
+     service unifiedviews-backend start
+     service unifiedviews-frontend start
+
+     # Create the repository (requires tomcat7 to be running)
+     pushd ../sesame-databases
+      sleep 10;
+      /vagrant/downloads/openrdf-sesame-2.8.6/bin/console.sh <<EOF
+create remote
+
+unifiedviews
+unifiedviews
+UnifiedViews Repository    
+exit
+EOF
+      echo "***** SESAME should be okay"
+     popd
+    popd
 else
     echo "***** SESAME openrdf-sesame-2.8.6-sdk.zip not downloaded"
 fi    
